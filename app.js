a250b928e4a3a7a6467beb5028ac73b011a14e46
Wade Stewart <stew.wade@gmail.com>
2018-06-01 00:16:14 -0400

    set up server, added dependencies

---
+++ app.js
@@ -0,0 +1,18 @@
+const express   = require('express')
+const hbs       = require('handle')
+
+const app = express()
+
+const PORT = 4001
+
+app.set('port', process.env.PORT || PORT)
+app.set('view engine', 'hbs')
+
+app.engine('.hbs', hbs({
+    extname:        '.hbs',
+    partialsDir:    'views/',
+    layoutsDir:     'views/',
+    defaultLayout:  'layout'
+}))
+
+app.listen(PORT, () => console.log(`Live on port ${PORT}`))
