d56fe21fcf20d2173a31bdef16f1e5550defee00
Wade Stewart <stew.wade@gmail.com>
2018-06-01 12:42:22 -0400

    rendering static content through controller

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express   = require('express')
 const hbs       = require('express-handlebars')
 const mongoose  = require('mongoose')
+const routes    = require('./config/routes')
 
 const app       = express()
 
-app.use(express.static(__dirname + '/public'))
 
 const PORT      = 4001
 
@@ -18,11 +18,8 @@ app.engine('.hbs', hbs({
     defaultLayout:  'layout'
 }))
 
-let routes = require('./config/routes')
 app.use(routes)
+app.use('/assets', express.static('/public'))
 
 app.listen(PORT, () => console.log(`Live on port ${PORT}`))
 
-app.get('/', (req, res) => {
-    res.render('welcome')
-})
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,10 @@
 const express   = require('express')
+const mongoose  = require('mongoose')
 const router    = express.Router()
 
-const staticsController = require('../controllers/static')
+const staticsController = require('../controllers/static.js')
 
-router.route('/', staticsController.home)
+router.route('/')
+    .get(staticsController.home)
 
 module.exports = router
