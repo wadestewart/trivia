dc19db68695978f96662c6f9c82f64c065b3e7d5
Wade Stewart <stew.wade@gmail.com>
2019-02-07 20:59:43 -0500

    fixed vulnerabilities and removed unused maongoose files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express   = require('express')
 const hbs       = require('express-handlebars')
-const mongoose  = require('mongoose')
+// const mongoose  = require('mongoose')
 const routes    = require('./config/routes')
 
 const app       = express()
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,5 @@
 const express   = require('express')
-const mongoose  = require('mongoose')
+// const mongoose  = require('mongoose')
 const router    = express.Router()
 
 const staticsController = require('../controllers/static.js')
